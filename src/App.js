8b2907b7378421a6cf223b982107ad977990e2d2
chrisolorobin <camorton88@gmail.com>
2020-11-18 13:39:20 -0800

    added alert that displays index of square

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
@@ -9,10 +10,30 @@ class App extends Component{
     }
   }
 
+  handleGamePlay = (index) => {
+    alert ( index )
+
+  }
+
   render(){
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        
+        <div id="gameboard">
+        { this.state.squares.map((value, index) => {
+          return (
+            <Square 
+              value = { value }
+              key = { index }
+              index = { index }
+              handleGamePlay = { this.handleGamePlay }
+            />
+          )
+        })
+        }
+        </div>
+
       </React.Fragment>
     )
   }
